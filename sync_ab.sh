0982389be8371311fd04ec24252bcc68df0ed68b
mulova <mulova@gmail.com>
2019-08-08 12:35:00 +0900

    move directory

--- sync_ab.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-DIR="Assets/AssetBundleManager"
-SRC="../../learn/AssetBundleDemo/demo/Assets/AssetBundleManager"
-LIST=("")
-
-for i in "${LIST[@]}"
-do
-    rsync -av --delete $SRC/${i} $DIR
-done
